c0b622e5df7abad61724d76889ef57e4734b788c
ridhoestu.b <ridhoestu.b@gmail.com>
2026-07-09 21:09:39 +0700

    style: update input corner radius and focus ring thickness

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -5,12 +5,12 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const inputVariants = cva(
-  'font-sans w-full min-w-0 rounded-lg border transition-all outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
+  'font-sans w-full min-w-0 rounded-md border transition-all outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
   {
     variants: {
       variant: {
         default:
-          'border-input bg-transparent focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50',
+          'border-input bg-transparent focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/50',
         primary:
           'border-outline-variant bg-surface-container-lowest focus-visible:border-primary focus-visible:ring-primary',
         filled:
