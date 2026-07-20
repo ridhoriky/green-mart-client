a7067b54e51b56d552e7b9209dcedb221e6b4937
ridhoestu.b <ridhoestu.b@gmail.com>
2026-07-20 23:26:15 +0700

    feat: implement wishlist page UI and translations

---
+++ src/features/wishlist/index.ts
@@ -0,0 +1,3 @@
+export { WishlistPage } from './components/WishlistPage';
+export { useWishlistQuery, useToggleWishlistMutation } from './hooks/useWishlist';
+export type { WishlistItem, WishlistResponse } from './types/wishlist';
